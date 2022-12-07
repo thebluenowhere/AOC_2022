abc36e94dfbde919dd6f7606ac7bce845a38a473
Michael Scott <thebluenowhere@protonmail.com>
2022-12-07 15:32:40 +0000

    Cleaning pt 1

--- Day02/Day02.py
+++ Day02/Day02.py
@@ -4,26 +4,26 @@ def main():
     score = 0
     f = open('input.txt', 'r')
     for line in f:
-        rounds = line
+        rounds = line.strip()
 
-        if rounds == "A X\n":
-            score = 3 + 1 + score
-        elif rounds == "A Y\n":
-            score = 6 + 2 + score
-        elif rounds == "A Z\n":
-            score = 0 + 3 + score
-        elif rounds == "B X\n":
-            score = 0 + 1 + score
-        elif rounds == "B Y\n":
-            score = 3 + 2 + score
-        elif rounds == "B Z\n":
-            score = 6 + 3 + score
-        elif rounds == "C X\n":
-            score = 6 + 1 + score
-        elif rounds == "C Y\n":
-            score = 0 + 2 + score
-        elif rounds == "C Z\n":
-            score = 3 + 3 + score
+        if rounds == "A X":
+            score += 3 + 1
+        elif rounds == "A Y":
+            score += 6 + 2
+        elif rounds == "A Z":
+            score += 0 + 3
+        elif rounds == "B X":
+            score += 0 + 1
+        elif rounds == "B Y":
+            score += 3 + 2
+        elif rounds == "B Z":
+            score += 6 + 3
+        elif rounds == "C X":
+            score += 6 + 1
+        elif rounds == "C Y":
+            score += 0 + 2
+        elif rounds == "C Z":
+            score += 3 + 3
 
     print(f"Part 1: {score}")
 
