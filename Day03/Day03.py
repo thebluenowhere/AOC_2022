c015e55fde7749c282be0f7fcf48514e874a028d
Michael Scott <thebluenowhere@protonmail.com>
2022-12-08 10:11:09 +0000

    day 3 done

--- Day03/Day03.py
+++ Day03/Day03.py
@@ -18,8 +18,17 @@ def part_one(contents):
     print(f"Part 1: {priority}")    
 
 def part_two(contents):
-    # 
-    pass
+    badge = 0
+    group = [contents[i:i + 3] for i in range(0, len(contents), 3)]
+    for trio in group:
+        for i in trio[0]:
+            if i in trio[1]:
+                if i in trio[2]:
+                    n = ord(i) - 96 if i.islower() else ord(i) - 38
+                    badge += n
+                    break
+    
+    print(f"Part 2: {badge}")
 
 def main(contents, part_one, part_two):
     part_one(contents)
